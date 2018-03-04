5cf75cd196bb2bb5b51e4ad1eeb81082013d783a
samidallell <dallellsami@gmail.com>
2018-03-04 02:54:51 +0100

    Notre premier commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users,
+               :path => '',
+               :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+  root 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
